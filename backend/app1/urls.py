dbcf8f5c16f4c98b6f9ce8929e44346ec0c6a4ef
annapoornanarayan <annapoorna.narayan@gmail.com>
2024-11-26 09:35:54 -0600

    Login integration

--- backend/app1/urls.py
+++ backend/app1/urls.py
@@ -1,19 +1,7 @@
 # app1/urls.py
 
-from django.urls import path, include
-from . import views
-from .views import index, LoginView
-#from .views import home, login_view, logout_view
 
-# urlpatterns = [
-#     path('', home, name='home'),
-#     path('login/', login_view, name='login'),
-#     path('logout/', logout_view, name='logout'),
-# ]
 
 urlpatterns = [
-    path('home/', views.index),
-    path('login/', LoginView.as_view(), name='login'),
 ]
 
-#path login page with the new class definition
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,34 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path('oauth/', include('social_django.urls', namespace='social')),
-#     path('', include('app1.urls')),  # Include your app's URLs
-# ]
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import TemplateView # useful in displaying index.html template
 from django.contrib.auth.views import LogoutView
-
+from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('email/', include('email_handler.urls')),
     path('backend/',include('app1.urls')), #app_auth urls
 
+    path('login/google/', views.custom_login, name='custom_login'),
+    path('login/', views.custom_login, name='login'),
+    path('logout', LogoutView.as_view()), # default Django logout view at /logout
+
     path('', TemplateView.as_view(template_name='index.html')),
     path('accounts/', include('allauth.urls')), # all OAuth operations will be performed under this route
 
-    path('logout', LogoutView.as_view()), # default Django logout view at /logout
+    
     path('pl/', include('scraper.urls')),
 
-    #path('assignments/', get_all_assignments, name='get_all_assignments'),
-    #path('courses/<int:course_id>/assignments/', get_assignments_by_course, name='get_assignments_by_course'),
-    #path('assignments/<int:assignment_id>/', get_assignment_by_id, name='get_assignment_by_id'),
-    #path('login/', canvas_login, name='canvas_login'),
-    #path('callback/', canvas_callback, name='canvas_callback'),
+    path('assignments/', get_all_assignments, name='get_all_assignments'),
+    path('courses/<int:course_id>/assignments/', get_assignments_by_course, name='get_assignments_by_course'),
+    path('assignments/<int:assignment_id>/', get_assignment_by_id, name='get_assignment_by_id'),
+    path('login/', canvas_login, name='canvas_login'),
+    path('callback/', canvas_callback, name='canvas_callback'),
     path('canvasapp/', include('canvasapp.urls'))
 ]
